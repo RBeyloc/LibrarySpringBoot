5fa5a8b5b400f690c593c60c65b7dd4a5bc0a8eb
RBeyloc <102668219+RBeyloc@users.noreply.github.com>
2022-05-23 12:45:18 +0200

    First Commit

---
+++ src/main/java/io/company/library/BookRepository.java
@@ -0,0 +1,14 @@
+package io.company.library;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository <Book, Long> {
+    //
+    Optional<Book> findBookByTitle(String title);
+
+    Optional<Book> deleteBookByTitle(String title);
+    // Optional<Book> createBook (Book book);
+}
+
