b1ee841563ab7caea6f1f8238906a4867672246c
RBeyloc <102668219+RBeyloc@users.noreply.github.com>
2022-05-23 13:55:42 +0200

    Semi implemented LibraryREstController

--- src/main/java/io/company/library/LibraryRestController.java
+++ src/main/java/io/company/library/LibraryRestController.java
@@ -1,6 +1,7 @@
 package io.company.library;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,36 +18,37 @@ public class LibraryRestController {
     //CRUD: read
     @GetMapping("books")
     public Iterable<Book> getAllBooks() {
-        //
         return bookservice.getAllBooks();
     }
 
     //CRUD: read, find one book by id
     @GetMapping("getBook")
-    public Book findBookById(Long id){
-
+    public Optional<Book> findBookById(Long id){
         Optional<Book> bookFound = bookservice.findBookById(id);
-        if (bookFound.isPresent()) return  bookFound.get();
-
-        return null;
+        if (bookFound.isPresent()) {
+            return Optional.of(bookFound.get());
+        } else  {
+            return null;
+        }
     }
 
     //CRUD: create
     @PostMapping("addBook")
-    public Book addBook(Book book){
-
+    public Book addBook(@RequestBody Book book){
         Book bookCreated = bookservice.createBook(book);
-
         return bookCreated ;
     }
 
     //CRUD: delete
     @DeleteMapping("deleteBook")
-    public Book deleteBook (Long id){
-
-        bookservice.deleteBookById(id);
-
-        return null;
+    public Optional<Book> deleteBook (Long id){
+        Optional<Book> bookFound = bookservice.findBookById(id);
+        if(bookFound.isPresent()) {
+            bookservice.deleteBookById(id);
+            return Optional.of(bookFound.get());
+        } else {
+            return null;
+        }
     }
 
 
